2c12f82667463ae7aa9da1260c1a479014339ee2
Daniil Beloglazov <33455333+Zheltog@users.noreply.github.com>
2026-07-09 19:49:24 +0700

    fix?

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,12 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
 kotlin {
     jvmToolchain(8)
 }
